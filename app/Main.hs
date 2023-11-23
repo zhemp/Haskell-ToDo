bfdad330d4017cd8771822d528bf260cb9fcbaf6
Zihan Lyu <zihan@Zihans-MacBook-Air.local>
2023-11-22 19:07:23 -0800

    hide warning

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
 module Main where
 
 import Brick
 
--- >>> 1+1
+-- >>> 1+2
+-- 3
 
 ui :: Widget ()
 ui = str "Hello, world!"
 
 main :: IO ()
 main = simpleMain ui
+
