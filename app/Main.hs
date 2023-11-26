ef8e23318f978228ebcc68e82220d59d8512ab84
Zihan Lyu <zihan@Zihans-MacBook-Air.local>
2023-11-25 21:43:34 -0800

    fixed the bug that some package can not be imported

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,104 @@
-{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Brick
+#if !(MIN_VERSION_base(4,11,0))
+import Data.Monoid ((<>))
+#endif
 
--- >>> 1+2
--- 3
+import qualified Data.Text as T
+import qualified Graphics.Vty as V
+
+import qualified Brick.Main as M
+import Brick.Util (fg, on)
+import qualified Brick.AttrMap as A
+import Brick.Types
+  ( Widget
+  )
+import Brick.Widgets.Core
+  ( (<+>)
+  , withAttr
+  , vLimit
+  , hLimit
+  , hBox
+  , vBox
+  , updateAttrMap
+  , withBorderStyle
+  , txt
+  , str
+  , padLeftRight
+  )
+import qualified Brick.Widgets.Center as C
+import qualified Brick.Widgets.Border as B
+import qualified Brick.Widgets.Border.Style as BS
+
+styles :: [(T.Text, BS.BorderStyle)]
+styles =
+    [ ("ascii", BS.ascii)
+    , ("unicode", BS.unicode)
+    , ("unicode bold", BS.unicodeBold)
+    , ("unicode rounded", BS.unicodeRounded)
+    , ("custom", custom)
+    , ("from 'x'", BS.borderStyleFromChar 'x')
+    ]
+
+custom :: BS.BorderStyle
+custom =
+    BS.BorderStyle { BS.bsCornerTL = '/'
+                   , BS.bsCornerTR = '\\'
+                   , BS.bsCornerBR = '/'
+                   , BS.bsCornerBL = '\\'
+                   , BS.bsIntersectFull = '.'
+                   , BS.bsIntersectL = '.'
+                   , BS.bsIntersectR = '.'
+                   , BS.bsIntersectT = '.'
+                   , BS.bsIntersectB = '.'
+                   , BS.bsHorizontal = '*'
+                   , BS.bsVertical = '!'
+                   }
+
+borderDemos :: [Widget ()]
+borderDemos = mkBorderDemo <$> styles
+
+mkBorderDemo :: (T.Text, BS.BorderStyle) -> Widget ()
+mkBorderDemo (styleName, sty) =
+    withBorderStyle sty $
+    B.borderWithLabel (str "label") $
+    vLimit 5 $
+    C.vCenter $
+    padLeftRight 2 $
+    txt $ styleName <> " style"
+
+titleAttr :: A.AttrName
+titleAttr = A.attrName "title"
+
+attrs :: [(A.AttrName, V.Attr)]
+attrs =
+    [ (B.borderAttr,         V.yellow `on` V.black)
+    , (B.vBorderAttr,        fg V.cyan)
+    , (B.hBorderAttr,        fg V.magenta)
+    , (titleAttr,            fg V.cyan)
+    ]
+
+colorDemo :: Widget ()
+colorDemo =
+    updateAttrMap (A.applyAttrMappings attrs) $
+    B.borderWithLabel (withAttr titleAttr $ str "title") $
+    hLimit 20 $
+    vLimit 5 $
+    C.center $
+    str "colors!"
 
 ui :: Widget ()
-ui = str "Hello, world!"
+ui =
+    vBox [ hBox borderDemos
+         , B.hBorder
+         , colorDemo
+         , B.hBorderWithLabel (str "horizontal border label")
+         , (C.center (str "Left of vertical border")
+             <+> B.vBorder
+             <+> C.center (str "Right of vertical border"))
+         ]
 
 main :: IO ()
-main = simpleMain ui
-
+main = M.simpleMain ui
